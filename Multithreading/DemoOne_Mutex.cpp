cd89d1e988cc5b7f659081e883b39ea93c8488a3
Abduraimov Muminjon <muminjonguru@outlook.com>
2023-11-20 22:21:04 +0000

    Mutex plus Counter sample
    no need to unlock, guard does the job.

---
+++ Multithreading/DemoOne_Mutex.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <mutex>
+
+std::mutex mtx;
+int counter = 0;
+
+void increment_counter(int iterations) {
+    for (int i = 0; i < iterations; ++i) {
+        std::lock_guard<std::mutex> lock(mtx);
+        ++counter;
+    }
+}
+
+int main() {
+    const int num_threads = 10;
+    const int iterations_per_thread = 1000;
+
+    std::vector<std::thread> threads;
+    for (int i = 0; i < num_threads; ++i) {
+        threads.push_back(std::thread(increment_counter, iterations_per_thread));
+    }
+
+    for (auto& thread : threads) {
+        thread.join();
+    }
+
+    std::cout << "Counter value: " << counter << std::endl;
+
+    return 0;
+}
